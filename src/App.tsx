ef0e21462c0ec89d206a923369fe0c534784fad2
rahmanabdulquadir <rahmanaq777@gmail.com>
2024-08-24 21:43:03 +0600

    initial setup done

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1>Assignment 5</h1>
+      <Button>This is shadcn button</Button>
+    </>
+  );
+}
+
+export default App;

d832ed6ce11cc8f913ecceab3e5a47e5c4450552
rahmanabdulquadir <rahmanaq777@gmail.com>
2024-10-27 19:16:15 +0600

    slightly info changed

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -145,7 +145,7 @@ const Login = () => {
                       type="button"
                       className="w-fit px-4 py-2 bg-gray-200 text-black rounded-md hover:bg-gray-300"
                       onClick={() => {
-                        setFieldValue("email", "admin@gmail.com");
+                        setFieldValue("email", "mradmin@gmail.com");
                         setFieldValue("password", "admin123");
                       }}
                     >
